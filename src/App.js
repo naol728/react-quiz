546c0d7616e21b60f2ba1be8523e87c9f15adc68
naol <naolmeseret22@gmail.com>
2024-08-18 18:14:00 +0300

    i add some componets and i have also use useReducer hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,62 @@
+import {useEffect,useReducer} from 'react';
+import './index.css';
+import Header from './Header'
+import Main from './Main'
+import Loader from './Loader'
+import Error from './Error'
+import StartScreen from './StartScreen';
 
-import './App.css';
+const initalstate={
+ quations:[],
+//   'loading' 'ready' 'active' 'error'
+ status:'loading'
 
+}
+function reducer (state,action){
+switch(action.type){
+  case 'dataRecived':
+    return {
+      ...state,
+       quations:action.payload,
+       status:"ready"
+    }
+    case 'error':
+      return {
+        ...state,
+        status:'error'
+      }
+  default :
+   throw new Error("Action unkown")
+}
+}
 function App() {
+   const [{status ,quations},dispach]=useReducer(reducer,initalstate);
+  useEffect(
+    function (){
+      
+        async function datafetching(){
+      
+          try{  const res=await fetch("http://localhost:8000/questions");
+        const data=await res.json();
+        dispach({type:'dataRecived',payload:data})
+      }
+       catch(err){
+        dispach({type:'error'})
+       }
+     }
+     datafetching();
+    },[]
+  )
+
   return (
     <div className="App">
-    
+    <Header />
+      
+      <Main>
+         {status==="loading" && <Loader />}
+         {status==="error" && <Error />}
+         {status==="ready" && <StartScreen />}
+      </Main>
     </div>
   );
 }
---
+++ src/StartScreen.js
@@ -0,0 +1,11 @@
+import './index.css'
+export default function StartScreen(){
+    return (
+        <div className="start">
+           <h2>Wellcome to The React Quiz </h2>
+           <h3>x questions to test your react Mastery</h3>
+
+           <button>Let's Start</button>
+        </div>
+    )
+}
